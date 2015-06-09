3912b99e0b71007a94beb4e5197dc5d9e9d470b7
Cohen Raphaël <raphael.cohen@jcdecaux.fr>
2015-06-09 16:46:28 +0200

    added tests

---
+++ test/main.js
@@ -0,0 +1,19 @@
+describe("Load libs synchronously and check callback result: 2 libs succes and 1 fail (in that order)", function () {
+    var libsLoadedResult;
+
+    beforeEach(function (done) {
+        var libsLoader = new OrderedLibLoader();
+        libsLoader.addLib("https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js");
+        libsLoader.addLib("https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js");
+        libsLoader.addLib("http://failfailfailfailfailfailfail.js");
+        libsLoader.loadAllLibs(function (result) {
+            libsLoadedResult = result;
+            done();
+        });
+    });
+
+    it("adds 3 libs, first 2 should load, last 1 should fail", function () {
+        expect(JSON.stringify(libsLoadedResult)).toEqual('[{"url":"https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js","status":"loaded"},{"url":"https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js","status":"loaded"},{"url":"http://failfailfailfailfailfailfail.js","status":"failed"}]');
+    });
+
+});
